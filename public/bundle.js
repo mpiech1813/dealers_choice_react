43bed4236d80e724ce0bdcaf553e60fa1f299616
Maciej Pie <maciejpiech18@gmail.com>
2021-03-10 12:00:33 -0600

    added webpack and babel

---
+++ public/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./app/index.js ***!
+  \**********************/
+//react will go here
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const syncAndSeed = require('./db/syncAndSeed');
+const morgan = require('morgan');
 
 const app = express();
 const { static } = express;
 
 app.use('/public', static(path.join(__dirname, './public')));
+app.use('/api', require('./routes/index'));
+app.use(morgan('dev'));
 
 app.get('/', async (req, res, next) => {
   try {
@@ -15,14 +18,20 @@ app.get('/', async (req, res, next) => {
   }
 });
 
-app.use('/api', require('./routes/index'));
-
 const run = async () => {
   try {
     await syncAndSeed();
 
     const PORT = process.env.PORT || 1813;
-    app.listen(PORT, () => console.log(`app listening on ${PORT}`));
+    app.listen(PORT, () =>
+      console.log(`
+    
+    app listening on ${PORT}
+    
+    http://localhost:${PORT}/
+
+    `)
+    );
   } catch (error) {
     console.log(error);
   }
